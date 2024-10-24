d12373db8e6c95ee1c9d1ee0b523246bf4380a0a
Roc-420 <nlast031@gmail.com>
2024-10-24 10:28:00 -0400

    create function for determinining possible moves

--- maze.py
+++ maze.py
@@ -1,5 +1,5 @@
 import csv
-
+from colorama import Fore
 
 def get_maze(): # converts maze txt into array
     lister = []
@@ -10,10 +10,10 @@ def get_maze(): # converts maze txt into array
     for row in content:
         for item in row:
             temp_row.append(item)
+        temp_row.pop(-1)
         lister.append(temp_row)
         temp_row = []
-            
-
+    
     return lister
 
 mazes = get_maze()
@@ -24,7 +24,16 @@ def print_maze(maze):
         for item in row:
             print(item,end="")
 
+def possible_moves(point_y,point_x, maze):
+    possible = []
+    if maze[(point_y + -1 )][point_x] == ' ': # one above current point
+        possible.append("up")
+    if  maze[(point_y + 1 )][point_x] == ' ':
+        possible.append("down")
+    if maze[point_y][point_x - 1 ] == ' ':
+        possible.append("left")
+    if maze[point_y][point_x + 1] == ' ':
+        possible.append("right")
 
+    return possible
 
-
-print_maze(mazes)
